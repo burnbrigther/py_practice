a19fa6f2f1e3f69a63994551d5a4130bfe72e995
Kurt Scholtens <burnbrighter@hotmail.com>
2015-09-01 22:16:08 +0100

    First time using for loop in python.  This is practice item 2 in exercise 16.

---
+++ ex16_2.py
@@ -0,0 +1,21 @@
+# import argv from the system module
+# allows us to take info from the command line and make it
+# available to the program
+from sys import argv
+# argv will expect two input variables from CL.  First will
+# be the name of the script and the other will be the filename
+# we will be working with
+script, filename = argv
+
+print "Opening the file..."
+# filename is open and readable via the 'r' parameter and assigned to the variable (or object) target
+target = open(filename, 'r')
+
+# Now we create a for loop in which we circulate through all lines in target, then we simply print
+# the lines out.
+for line in target:
+    print line,
+
+# Finally, clean up.  Good programmer
+print "And finally, we close it."
+target.close()
