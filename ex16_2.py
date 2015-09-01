5290499126b0b718d2372e2332c3f918844dc35d
Kurt Scholtens <burnbrighter@hotmail.com>
2015-09-01 12:38:20 +0100

    a few little changes

--- ex16_2.py
+++ ex16_2.py
@@ -1,3 +1,5 @@
 # This is a comment only test
 # There is nothing of value in this file and its really just for testing
 # OK, I'm going to add and try to recommit
+# OK, I'm going to add and try to recommit OK, I'm going to add and try to recommit
+# SO YET ANOTHER TEST
